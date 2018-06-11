9e6328c85fbdbab58d9a913e50e91fdaf524aa93
Daniel Ridgway <ridginald@gmail.com>
2018-06-11 16:36:58 +0100

    added 3 and 5 spec and functions

--- jasmine-standalone-3.1.0/spec/fizzBuzzSpec.js
+++ jasmine-standalone-3.1.0/spec/fizzBuzzSpec.js
@@ -14,4 +14,32 @@ describe('Javabuzz', function() {
       });
     });
   });
+
+  describe('knows when a number is', function() {
+    it('divisble by 5', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
+    describe('when a number is NOT', function() {
+      it('divisble by 5', function() {
+        javabuzz = new Javabuzz();
+        expect(javabuzz.isDivisibleByFive(1)).toBe(false);
+      });
+    });
+  });
+
+  describe('knows when a number is', function() {
+    it('divisble by 3 and 5', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThreeAndFive(15)).toBe(true);
+    });
+
+    describe('when a number is NOT', function() {
+      it('divisble by 3 and 5', function() {
+        javabuzz = new Javabuzz();
+        expect(javabuzz.isDivisibleByThreeAndFive(1)).toBe(false);
+      });
+    });
+  });
 });
--- jasmine-standalone-3.1.0/src/fizzBuzz.js
+++ jasmine-standalone-3.1.0/src/fizzBuzz.js
@@ -7,3 +7,19 @@ Javabuzz.prototype.isDivisibleByThree = function(number) {
 Javabuzz.prototype.isDivisibleByThree = function(number) {
   return(number % 3 === 0);
 };
+
+Javabuzz.prototype.isDivisibleByFive = function(number) {
+  return true;
+};
+
+Javabuzz.prototype.isDivisibleByFive = function(number) {
+  return(number % 5 === 0);
+};
+
+Javabuzz.prototype.isDivisibleByThreeAndFive = function(number) {
+  return true;
+};
+
+Javabuzz.prototype.isDivisibleByThreeAndFive = function(number) {
+  return(number % 15 === 0);
+};
