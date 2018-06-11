dcabe10e5499854d6318d8b828552d792e2dd010
Daniel Ridgway <ridginald@gmail.com>
2018-06-11 16:39:54 +0100

    refactor beforeEach

--- jasmine-standalone-3.1.0/spec/fizzBuzzSpec.js
+++ jasmine-standalone-3.1.0/spec/fizzBuzzSpec.js
@@ -1,15 +1,17 @@
 describe('Javabuzz', function() {
   var javabuzz;
 
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  })
+
   describe('knows when a number is', function() {
     it('divisble by 3', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThree(3)).toBe(true);
     });
 
     describe('when a number is NOT', function() {
       it('divisble by 3', function() {
-        javabuzz = new Javabuzz();
         expect(javabuzz.isDivisibleByThree(1)).toBe(false);
       });
     });
@@ -17,13 +19,11 @@ describe('Javabuzz', function() {
 
   describe('knows when a number is', function() {
     it('divisble by 5', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByFive(5)).toBe(true);
     });
 
     describe('when a number is NOT', function() {
       it('divisble by 5', function() {
-        javabuzz = new Javabuzz();
         expect(javabuzz.isDivisibleByFive(1)).toBe(false);
       });
     });
@@ -31,13 +31,11 @@ describe('Javabuzz', function() {
 
   describe('knows when a number is', function() {
     it('divisble by 3 and 5', function() {
-      javabuzz = new Javabuzz();
       expect(javabuzz.isDivisibleByThreeAndFive(15)).toBe(true);
     });
 
     describe('when a number is NOT', function() {
       it('divisble by 3 and 5', function() {
-        javabuzz = new Javabuzz();
         expect(javabuzz.isDivisibleByThreeAndFive(1)).toBe(false);
       });
     });
